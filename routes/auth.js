557805b3c3f96b24fbbc0c28758d04bd6d27c186
pavitpim40 <pavitpim@gmail.com>
2022-04-07 02:08:03 +0700

    Finish Login

--- routes/auth.js
+++ routes/auth.js
@@ -23,4 +23,22 @@ router.post("/register", async (req, res) => {
 		console.log(error);
 	}
 });
+
+// LOGIN
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		!user && res.status(404).json({ message: "User not found" });
+
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		!validPassword && res.status(400).json({ message: "Invalid password" });
+
+		res.status(200).json(user);
+	} catch (error) {
+		console.log(error);
+	}
+});
 module.exports = router;
