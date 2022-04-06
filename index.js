705e454126afa883764192701c60f4df1faf376c
pavitpim40 <pavitpim@gmail.com>
2022-04-07 01:10:35 +0700

    setup route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -21,13 +23,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-	res.send("welcome to homepage");
-});
-
-app.get("/users", (req, res) => {
-	res.send("welcome to user page");
-});
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
 	console.log("Backend server is running");
