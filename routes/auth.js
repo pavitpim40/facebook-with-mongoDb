df9979396be995cb72042855a4b8652a0b692e42
pavitpim40 <pavitpim@gmail.com>
2022-04-12 12:32:23 +0700

    delete user

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.post("/register", async (req, res) => {
 		res.status(200).json(user);
 	} catch (error) {
 		console.log(error);
-		res.status(500).json(err);
+		res.status(500).json(error);
 	}
 });
 
--- routes/users.js
+++ routes/users.js
@@ -32,4 +32,18 @@ router.put("/:id", async (req, res) => {
 		res.status(403).json("You can update only your account");
 	}
 });
+
+// delete user
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findByIdAndDelete(req.params.id);
+			res.status(200).json("Account has been deleted");
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json("You can delete only your account");
+	}
+});
 module.exports = router;
